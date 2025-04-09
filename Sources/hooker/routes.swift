5d8011db21d8f68224ff3a8f6b305fc06bb9d54d
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2025-04-09 14:11:04 -0500

    wip

--- Sources/hooker/routes.swift
+++ Sources/hooker/routes.swift
@@ -17,7 +17,34 @@ func routes(_ app: Application) throws {
     
     app.post("webhooks") { req async -> String in
         req.logger.info("post webhooks \(req.body)")
+        Task {
+            try await executeProcessAndReturnResult("swift -v")
+        }
+        
         return ""
+    }
+    
+    func executeProcessAndReturnResult(_ command: String) -> String {
+        let process = Process()
+        let pipe = Pipe()
+        let environment = [
+            "TERM": "xterm",
+            "HOME": "/Users/example-user/",
+            "PATH": "/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+        ]
+        process.standardOutput = pipe
+        process.standardError = pipe
+//        process.environment = environment
+        process.launchPath = "/bin/zsh"
+        process.arguments = ["-c", command]
+        if #available(macOS 13.0, *) {
+            try! process.run()
+        } else {
+            process.launch()
+        }
+        let data = pipe.fileHandleForEading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8) ?? ""
+        return output
     }
     
     try app.register(collection: TodoController())
